a97452f349d84afc80dad64fdb0846453ab231ce
RAKIB <rakibmahmudmridha@gmail.com>
2023-11-05 21:07:05 +0600

    files added and modified

--- pollController.js
+++ pollController.js
@@ -22,7 +22,39 @@ exports.createPollPostController = async (req, res, next) => {
   try {
     await poll.save();
     res.redirect("/polls");
+    next();
   } catch (err) {
     console.log(err);
   }
+};
+
+exports.getAllPolls = async (req, res, next) => {
+  try {
+    let polls = await Poll.find();
+    res.render("polls", { polls });
+    next();
+  } catch (error) {}
+};
+
+exports.viewPollGetController = async (req, res, next) => {
+  let id = req.params.id;
+
+  try {
+    let poll = await Poll.findById(id);
+
+    let options = [...poll.options];
+    console.log(options);
+    let result = [];
+
+    options.forEach((option) => {
+      let parentage = (option.vote * 100) / poll.totalVote;
+
+      result.push({
+        ...option._doc,
+        parentage: parentage ? parentage : 0,
+      });
+    });
+    res.render("viewPoll", { poll, request });
+    next();
+  } catch (err) {}
 };
